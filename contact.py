3113df450422606328e6a0148bc3e3f4a641bf5c
despirin <play_with@list.ru>
2021-06-06 21:37:53 +0300

    добавлен кейс создание контакта

---
+++ contact.py
@@ -0,0 +1,9 @@
+
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nickname, company, title):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.company = company
+        self.title = title
