9c0424b8343b735a3f57474e14852341d46705ce
despirin <play_with@list.ru>
2021-06-15 18:29:14 +0300

    assert

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -3,6 +3,10 @@
 from model.group import Group
 
 def test_del_first_group(app):
-    if app.group.count () ==0:
+    if app.group.count () == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.test_del_first_group()
+# получить новый список групп
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) - 1 == len (new_groups)
