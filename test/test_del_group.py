c98b90c78dcf26f3f110eef8090e3db691c3bd64
despirin <play_with@list.ru>
2021-06-16 14:55:17 +0300

    добавлена проверка сравнение списков при удалении группы

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -10,3 +10,6 @@ def test_del_first_group(app):
 # получить новый список групп
     new_groups = app.group.get_group_list()
     assert len(old_groups) - 1 == len (new_groups)
+#
+    old_groups[0:1] = []
+    assert old_groups == new_groups
