84456e5a07af0de54abe05d3e3b8b0f5d151cb28
despirin <play_with@list.ru>
2021-06-16 10:42:01 +0300

    вернул уничтожение session

--- conftest.py
+++ conftest.py
@@ -15,12 +15,12 @@ def app(request):
     fixture.session.ensure_login("admin", "secret")
     return fixture
 
-# @pytest.fixture(scope = "session", autouse = True)
-# def stop(request):
-#     def fin():
-#         fixture.session.ensure_logout()
-#         fixture.destroy()
+@pytest.fixture (scope = "session", autouse = True)
+def stop(request):
+    def fin():
+        fixture.session.ensure_logout()
+        fixture.destroy()
 
 
-#     request.addfinalizer(fin)
-#     return fixture
+    request.addfinalizer(fin)
+    return fixture
