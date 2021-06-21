33768cf03f17db0a40a552b807775acc91aff6d4
despirin <play_with@list.ru>
2021-06-21 19:17:55 +0300

    111

--- conftest.py
+++ conftest.py
@@ -1,20 +1,28 @@
+import json
+import os.path
 import pytest
 from fixture.application import Application
 
 fixture = None
+taget = None
 
 @pytest.fixture #(scope = "session")
 def app(request):
     global fixture
+    global taget
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:
+    if taget is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+            base_url = target['baseUrl']
+            username = target['username']
+            password = target['password']
+    #base_url = request.config.getoption("--baseUrl")
+    if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
 
-    fixture.session.ensure_login("admin", "secret")
+    fixture.session.ensure_login(username, password)
     return fixture
 
 @pytest.fixture (scope = "session", autouse = True)
@@ -30,4 +38,5 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/index.php")
+    #parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/index.php")
+    parser.addoption("--target", action="store", default="target.json")
